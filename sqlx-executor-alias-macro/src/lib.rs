44c9f240764aa0f57c2e81fa324ebe1b2d5c51ff
vkill <vkill.net@gmail.com>
2023-09-09 02:04:05 +0800

    Release sqlx-executor-alias-macro 0.2.0

--- sqlx-executor-alias-macro/src/lib.rs
+++ sqlx-executor-alias-macro/src/lib.rs
@@ -53,11 +53,29 @@ mod tests {
         async fn postgres_execute<'c, E: FooPgExecutor<'c>>(executor: E) {
             let _ = sqlx::query("select 1").execute(executor).await;
         }
+
         let pool = sqlx::postgres::PgPoolOptions::new()
             .connect_lazy("postgres://127.0.0.1:5432")
             .expect("");
         match timeout(Duration::from_millis(500), postgres_execute(&pool)).await {
             Ok(_) => {}
+            Err(_) => {
+                eprintln!("timeout")
+            }
+        }
+
+        match timeout(Duration::from_millis(500), pool.acquire()).await {
+            Ok(Ok(mut conn)) => {
+                match timeout(Duration::from_millis(500), postgres_execute(conn.as_mut())).await {
+                    Ok(_) => {}
+                    Err(_) => {
+                        eprintln!("timeout")
+                    }
+                }
+            }
+            Ok(Err(_)) => {
+                eprintln!("acquire failed")
+            }
             Err(_) => {
                 eprintln!("timeout")
             }
